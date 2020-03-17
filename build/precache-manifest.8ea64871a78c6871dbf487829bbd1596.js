c106f145763f5ba3f29d8557b7489fa39facc959
Zachary Luther <Zluther89@gmail.com>
2020-03-17 14:36:03 -0400

    re-added build

---
+++ build/precache-manifest.8ea64871a78c6871dbf487829bbd1596.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d6ecbf91a22b6ec648c4bea435ec0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21b0ba4d6391a88a46c8",
+    "url": "/static/css/main.4c9bd5af.chunk.css"
+  },
+  {
+    "revision": "184b0f82e04430b4f964",
+    "url": "/static/js/2.e8d6ea97.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e8d6ea97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b0ba4d6391a88a46c8",
+    "url": "/static/js/main.37b7b25e.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
