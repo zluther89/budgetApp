fd7605cf940e55e6cdd275273b4cb457786c1aed
Andrew Jyan <andrew.jyan4@gmail.com>
2020-05-12 15:58:37 -0400

    added unit test suite

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+function sum(a, b) {
+  return a + b;
+}
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
